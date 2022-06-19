77ef7bb30150e679068294e8f98204ad433baf20
dipta <diptapanda7@gmail.com>
2022-06-19 12:47:40 +0530

    bug fixes

--- routes/auth.js
+++ routes/auth.js
@@ -2,16 +2,17 @@ const express = require('express');
 
 const authController = require('../controllers/auth');
 
+
 const router = express.Router();
 
 router.get('/login', authController.getLogin);
 
 router.get('/signup', authController.getSignup);
 
-router.post('/login', authController.postLogin);
+router.post('/login',  authController.postLogin);
 
-router.post('/signup', authController.postSignup);
+router.post('/signup',  authController.postSignup);
 
-router.post('/logout', authController.postLogout);
+router.post('/logout',  authController.postLogout);
 
 module.exports = router;
